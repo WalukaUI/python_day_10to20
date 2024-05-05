fe41e79f1b0cf439f74a8d213c7c2bc4b18161fb
walukaui <cwaluka@yahoo.com>
2024-05-05 16:58:06 -0500

    "day26"

--- name_the_state/main.py
+++ name_the_state/main.py
@@ -9,25 +9,40 @@ screen.addshape(image)
 turtle.shape(image)
 is_game_on = True
 datas = pandas.read_csv("./50_states.csv")
-states = datas["state"]
+states = datas["state"].to_list()
 correct_answers = []
+missing_states = []
+
 
 while is_game_on:
-    answer_box = screen.textinput("State name", "Enter a state")
-    for state in states:
-        if state == answer_box:
-            correct_answers.append(state)
-        else:
-            is_game_on = False
+    answer_box = screen.textinput(f'State name {len(correct_answers)}/50', "Enter a state").title()
+    if answer_box == "Exit":
+        for sta in states:
+            if sta not in correct_answers:
+                missing_states.append(sta)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states to learn.csv")
+        is_game_on = False
+    if answer_box in states:
+        correct_answers.append(answer_box)
+        t = Turtle()
+        t.penup()
+        t.hideturtle()
+        totai = datas[datas["state"] == answer_box]
+        t.goto(int(totai["x"]), int(totai["y"]))
+        t.write(answer_box)
+
+
 
 print(correct_answers)
+print(missing_states)
 
 
 
 
 
 
-turtle.mainloop()
+# turtle.mainloop()
 # screen.exitonclick()
 
 
