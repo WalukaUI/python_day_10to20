c2233f4fa46c13d12d8a77e44f0c956f0e805f4c
walukaui <cwaluka@yahoo.com>
2024-05-08 11:09:21 -0500

    "basic features of Tk"

--- GUI_day27/main.py
+++ GUI_day27/main.py
@@ -4,13 +4,32 @@ window = tkinter.Tk()
 window.title("Program GUI")
 window.minsize(800, 700)
 
-
 my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "italic"))
-my_label.pack(side="left")
+my_label.pack()
+my_label["text"] = "new text"
+
+#Button
+
+def button_clicked():
+    val = input.get()
+    my_label["text"] = val
+    print("clicked")
 
 
+button = tkinter.Button(text="clickme", command=button_clicked)
+button.pack()
+
+
+#Entry/Input
+
+input = tkinter.Entry(background="red", cursor="hand2")
+input.pack()
 
 
 
 window.mainloop()
+
+
+
+
 
