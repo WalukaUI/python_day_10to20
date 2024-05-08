0509ffb393c9615a4f2c9a16bde64eeab2863019
walukaui <cwaluka@yahoo.com>
2024-05-08 13:51:13 -0500

    "miles to km"

--- GUI_day27/main.py
+++ GUI_day27/main.py
@@ -3,9 +3,10 @@ import tkinter
 window = tkinter.Tk()
 window.title("Program GUI")
 window.minsize(800, 700)
+window.config(padx=20, pady=20)
 
 my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "italic"))
-my_label.pack()
+my_label.grid(column=0, row=0)
 my_label["text"] = "new text"
 
 #Button
@@ -17,13 +18,15 @@ def button_clicked():
 
 
 button = tkinter.Button(text="clickme", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
 
+button2 = tkinter.Button(text="button2", command=button_clicked)
+button2.grid(column=3, row=0)
 
 #Entry/Input
 
 input = tkinter.Entry(background="red", cursor="hand2")
-input.pack()
+input.place(x=200, y=200)
 
 
 
